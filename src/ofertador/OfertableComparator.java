aefa3bb3a5793d508e0ce3935ce80669f6ea2387
Cristian Cespedes <80497000+pacovant@users.noreply.github.com>
2023-06-12 21:17:18 -0300

    Se quita comentarios en ofertableComparator

--- src/ofertador/OfertableComparator.java
+++ src/ofertador/OfertableComparator.java
@@ -13,19 +13,6 @@ public class OfertableComparator implements Comparator<Ofertable> {
 
 	@Override
 	public int compare(Ofertable o1, Ofertable o2) {
-
-//		if (o1.getTipo().equals(preferencia)) {
-//			if (o2.getTipo().equals(preferencia)) {
-//				return compararTipo(o1, o2);
-//			} else
-//				return -1;
-//		} else {
-//			if (o2.getTipo().equals(preferencia)) {
-//				return 1;
-//			} else
-//				return compararTipo(o1, o2);
-//		}
-		
 		if (o1.getTipo().equals(preferencia) && o2.getTipo().equals(preferencia))
 			return compararTipo(o1, o2);
 		else if (o1.getTipo().equals(preferencia))
@@ -39,19 +26,7 @@ public class OfertableComparator implements Comparator<Ofertable> {
 			return 1;
 		if( o1.compareTo(o2) <= -1 )
 			return -1;
-		return compararPrecio(o1, o2);
-		
-//		IF( O1.GETCLASS().GETNAME().EQUALS("OFERTADOR.ATRACCION") ) {
-//			IF( O2.GETCLASS().GETNAME().EQUALS("OFERTADOR.ATRACCION") ) {
-//				RETURN COMPARARPRECIO(O1, O2);
-//			}ELSE
-//				RETURN 1;
-//		}ELSE {
-//			IF ( O2.GETCLASS().GETNAME().EQUALS("OFERTADOR.ATRACCION") ) {
-//				RETURN -1;
-//			} ELSE
-//				RETURN COMPARARPRECIO(O1, O2);
-//		}
+		return compararPrecio(o1, o2);	
 	}
 	
 	public int compararPrecio(Ofertable o1, Ofertable o2) {
